00b3824b2cdf7eeacf95964c241518a2e2920916
Ayla Tolosa-Kline <ayla_TK@Aylas-MacBook-Air.local>
2017-06-07 16:17:15 +0300

    BlocChat Checkpoint 4: COMPLETED

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,12 +1,17 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
-        this.heroTitle = "Chat like it's 1999!";
+    function HomeCtrl(Room, Message, $uibModal) {
+        this.heroTitle = "Chatting like it's 1999!";
         this.chatRooms = Room.all;
+        this.currentRoom = null;
       
+      this.setCurrentRoom = function (room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id);
+        }
        
         
     }
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', HomeCtrl]);
 })();
